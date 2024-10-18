219b68124eca0e7eb8f90840650155cf0c976ce7
ngthduongg623 <ngthduong623@gmail.com>
2024-10-18 18:39:04 +0700

    Update UI.java

--- src/main/UI.java
+++ src/main/UI.java
@@ -78,7 +78,7 @@ public class UI {
     }
     
     public void drawPlayerLife() {
-    	gp.player.life = 3;
+    	gp.player.life = gp.player.hasHeart;
     	
     	int x = gp.tileSize / 2;
     	int y = gp.tileSize / 2;
