3e5bd076dcdbdf62adb7cfa2799ef01966b5adb2
phudatdam <datdamphu@gmail.com>
2024-10-21 10:03:33 +0700

    fix: Restore player's speed

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -46,7 +46,7 @@ public class Player extends Entity {
     public void setDefaultValues() {
         worldX = gp.tileSize * 35;
         worldY = gp.tileSize * 13;
-        speed = 8;
+        speed = 5;
         direction = "down";
         
         maxLife = 6;
