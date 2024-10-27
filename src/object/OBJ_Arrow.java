1779f85a03abe7e013883704b62bf61a15893d10
Huy <nguyenvhuy2005@gmail.com>
2024-10-28 00:10:19 +0700

    fix: projectiles package

--- src/object/OBJ_Arrow.java
+++ src/object/OBJ_Arrow.java
@@ -21,10 +21,10 @@ public class OBJ_Arrow extends Projectile{
 	}
 	
 	public void getImage() {
-		up1 = up2 = setup("/projectile/arrow_up", gp.tileSize, gp.tileSize);
-		down1 = down2 = setup("/projectile/arrow_down", gp.tileSize, gp.tileSize);
-		right1 = right2 = setup("/projectile/arrow_right", gp.tileSize, gp.tileSize);
-		left1= left2 = setup("/projectile/arrow_left", gp.tileSize, gp.tileSize);
+		up1 = up2 = setup("/projectiles/arrow_up", gp.tileSize, gp.tileSize);
+		down1 = down2 = setup("/projectiles/arrow_down", gp.tileSize, gp.tileSize);
+		right1 = right2 = setup("/projectiles/arrow_right", gp.tileSize, gp.tileSize);
+		left1= left2 = setup("/projectiles/arrow_left", gp.tileSize, gp.tileSize);
 	}
 	
 	
