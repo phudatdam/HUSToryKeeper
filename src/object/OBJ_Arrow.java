bc60c0807f0aef4dd9b97a4d4997db0b3a71fd2d
Huy <nguyenvhuy2005@gmail.com>
2024-10-27 20:02:57 +0700

    feat: add projectile monster

---
+++ src/object/OBJ_Arrow.java
@@ -0,0 +1,31 @@
+package object;
+
+import entity.Projectile;
+import main.GamePanel;
+
+public class OBJ_Arrow extends Projectile{
+
+	GamePanel gp;
+	
+	public OBJ_Arrow(GamePanel gp) {
+		super(gp);
+		this.gp = gp;
+		
+		speed = 5; // tốc độ mũi tên bay
+		attack = 1;
+		maxLife = 75;
+		life = maxLife;
+		alive = false;
+		
+		getImage();
+	}
+	
+	public void getImage() {
+		up1 = up2 = setup("/projectile/arrow_up", gp.tileSize, gp.tileSize);
+		down1 = down2 = setup("/projectile/arrow_down", gp.tileSize, gp.tileSize);
+		right1 = right2 = setup("/projectile/arrow_right", gp.tileSize, gp.tileSize);
+		left1= left2 = setup("/projectile/arrow_left", gp.tileSize, gp.tileSize);
+	}
+	
+	
+}
