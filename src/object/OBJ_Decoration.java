6c3ab6204dbeff01d2b9f97089a792d3bcf8a7be
phudatdam <datdamphu@gmail.com>
2024-11-07 14:30:45 +0700

    refactor: Remove unused method

--- src/object/OBJ_Decoration.java
+++ src/object/OBJ_Decoration.java
@@ -9,7 +9,6 @@ public class OBJ_Decoration extends Entity {
 	
 	public OBJ_Decoration(GamePanel gp, String imagePath, int width, int height) {
 		super(gp);
-		name = "Well";
 		down1 = setup(imagePath, width, height);
 		
 		solidArea = new Rectangle (0, 0, width, height);
