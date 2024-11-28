efd2b44955d71d9fd08a2a50939d5bb62e82cba6
phudatdam <datdamphu@gmail.com>
2024-11-28 22:04:35 +0700

    refactor: Refactor final weapon classes

--- src/object/OBJ_IronHorse.java
+++ src/object/OBJ_IronHorse.java
@@ -9,12 +9,9 @@ public class OBJ_IronHorse extends Entity{
 		super(gp);
 
 		name = "Ngựa sắt";
-
-//		solidArea = new Rectangle(16, 16, 32, 32);
-		down2 = setup("/misc/iron_horse", 40, 40);
-
 		description = "[ " + name + " ]\nMột con ngựa được làm từ sắt, có thể\nphun ra lửa";
 		
 		image1 = setup("/objects/iron_horse", gp.tileSize, gp.tileSize);
+		image2 = setup("/misc/iron_horse", 96, 96);
 	}
 }
