d42734806c664f0da957dd32bb77dd48c3d9e19e
Dat Dam <datdamphu@gmail.com>
2024-12-05 14:57:56 +0700

    Merge branch feat/change-wall-graphics by phudatdam
    * refactor: Refactor final weapon classes
    
    * feat: Add weapon display
    
    * feat: Change wall tiles graphics in map 2 and 3

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -17,7 +17,7 @@ public class TileManager {
 
     public TileManager(GamePanel gp) {
         this.gp = gp;
-        tile = new Tile[90]; // set số lượng tile <= 90
+        tile = new Tile[130]; // set số lượng tile <= 130
         mapTileNum = new int[gp.maxMap][gp.maxWorldCol][gp.maxWorldRow];
 
         getTileImage();
@@ -46,6 +46,7 @@ public class TileManager {
     	setup(24, "path_corner_north_west", false); // góc đường hướng tây bắc
     	setup(25, "path_corner_south_east", false); // góc đường hướng đông nam
     	setup(26, "path_corner_south_west", false); // góc đường hướng tây nam
+    	setup(27, "stone_stairs", false); // cầu thang đá
     	// water
     	setup(30, "water", true); // nước
     	setup(31, "water_edge_east", true); // cạnh nước hướng đông
@@ -85,6 +86,32 @@ public class TileManager {
     	setup(78, "dorm_wall_corner_south_west", true); // góc tường phòng trọ hướng tây nam
     	setup(79, "dorm_wall_inner_corner_south_east", true); // góc tường trong phòng trọ hướng đông nam
     	setup(80, "dorm_wall_inner_corner_south_west", true); // góc tường trong phòng trọ hướng tây nam
+    	// bamboo wall
+    	setup(90, "bamboo_east", true); // tường tre hướng đông
+    	setup(91, "bamboo_west", true); // tường tre hướng tây
+    	setup(92, "bamboo_north", true); // tường tre hướng bắc
+    	setup(93, "bamboo_south", true); // tường tre hướng nam
+    	setup(94, "bamboo_corner_north_east", true); // tường tre hướng đông bắc
+    	setup(95, "bamboo_corner_north_west", true); // tường tre hướng tây bắc
+    	setup(96, "bamboo_corner_south_east", true); // tường tre hướng đông nam
+    	setup(97, "bamboo_corner_south_west", true); // tường tre hướng tây nam
+    	setup(98, "bamboo_inner_corner_north_east", true); // góc tường tre trong hướng đông bắc
+    	setup(99, "bamboo_inner_corner_north_west", true); // góc tường tre trong hướng tây bắc
+    	setup(100, "bamboo_inner_corner_south_east", true); // góc tường tre trong hướng đông nam
+    	setup(101, "bamboo_inner_corner_south_west", true); // góc tường tre trong hướng tây nam
+    	// stone wall
+    	setup(110, "stone_east", true); // tường đá hướng đông
+    	setup(111, "stone_west", true); // tường đá hướng tây
+    	setup(112, "stone_north", true); // tường đá hướng bắc
+    	setup(113, "stone_south", true); // tường đá hướng nam
+    	setup(114, "stone_corner_north_east", true); // tường đá hướng đông bắc
+    	setup(115, "stone_corner_north_west", true); // tường đá hướng tây bắc
+    	setup(116, "stone_corner_south_east", true); // tường đá hướng đông nam
+    	setup(117, "stone_corner_south_west", true); // tường đá hướng tây nam
+    	setup(118, "stone_inner_corner_north_east", true); // góc tường đá trong hướng đông bắc
+    	setup(119, "stone_inner_corner_north_west", true); // góc tường đá trong hướng tây bắc
+    	setup(120, "stone_inner_corner_south_east", true); // góc tường đá trong hướng đông nam
+    	setup(121, "stone_inner_corner_south_west", true); // góc tường đá trong hướng tây nam
     }
     
     // Công cụ vẽ hình tile
